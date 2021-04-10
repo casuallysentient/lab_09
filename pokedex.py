1a7d0bc57a2da771387e4f3d713bd463fe6b8fb6
casuallysentient <reilly.ford@gmail.com>
2021-04-10 13:46:46 -0400

    exercise 3

--- pokedex.py
+++ pokedex.py
@@ -26,6 +26,7 @@ def create_entry(number, name, type_1, type_2, health_points, attack, defense, s
     entry = {'Number': number, "Name": name, 'Types': types, 'Battle Stats': battle_stats, 'Generation': generation, 'Legendary': is_legendary}
     return entry
 
+
 def create_pokedex(filepath):
     """
     Creates a dictionary of Pokemon dictionary entries created from information extracted from file filepath.
@@ -33,7 +34,22 @@ def create_pokedex(filepath):
     :param filepath: A string containing the location of a file from which to read information from.
     :return: A dictionary of dictionaries.
     """
-    pass
+    try:
+        file = open(filepath, 'r')
+    except FileNotFoundError:
+        return {}
+    else:
+        pokedex = {}
+        for line in file:
+            stats = line.rstrip().split(",")
+            if stats[0].isdigit():
+                stats.pop(4)
+                if stats[11] == "True":
+                    legendary = True
+                else:
+                    legendary = False
+                pokedex[stats[1]] = create_entry(int(stats[0]), stats[1], stats[2], stats[3], int(stats[4]), int(stats[5]), int(stats[6]), int(stats[7]), int(stats[8]), int(stats[9]), int(stats[10]), legendary)
+        return pokedex
 
 
 def main():
@@ -52,22 +68,22 @@ def main():
 
     print()  # For formatting
     # P R O B L E M   3
-    # filepath = "pokemon.csv"
-    # pokedex = create_pokedex(filepath)
-    # pokemon_key = "Glaceon"
-    #
-    # # This is one of the many ways to check if a certain key exists in a dictionary!
-    # try:
-    #     # This step could potentially fail, so we "try" it first.
-    #     my_favorite_pokemon = pokedex[pokemon_key]
-    # except KeyError:
-    #     # If it does fail under a KeyError, we'll print an error message.
-    #     print("ERROR: Pokemon {} does not exist!".format(pokemon_key))
-    # else:
-    #     # If it doesn't fail under a KeyError, we'll print the Pokemon's info!
-    #     print("PRINTING {}'S INFORMATION...".format(pokemon_key))
-    #     for key in my_favorite_pokemon.keys():
-    #         print("{}: {}".format(key, my_favorite_pokemon[key]))
+    filepath = "pokemon.csv"
+    pokedex = create_pokedex(filepath)
+    pokemon_key = "Glaceon"
+
+    # This is one of the many ways to check if a certain key exists in a dictionary!
+    try:
+        # This step could potentially fail, so we "try" it first.
+        my_favorite_pokemon = pokedex[pokemon_key]
+    except KeyError:
+        # If it does fail under a KeyError, we'll print an error message.
+        print("ERROR: Pokemon {} does not exist!".format(pokemon_key))
+    else:
+        # If it doesn't fail under a KeyError, we'll print the Pokemon's info!
+        print("PRINTING {}'S INFORMATION...".format(pokemon_key))
+        for key in my_favorite_pokemon.keys():
+            print("{}: {}".format(key, my_favorite_pokemon[key]))
 
 
 # DO NOT WRITE CODE BELOW THIS LINE
